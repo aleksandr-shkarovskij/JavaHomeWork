d198c36a8ddcface6625a109dafc7cc5b874d10e
Aleksandr Shkarovskyi <aleksandr.shkarovskij@privatbank.ua>
2021-10-14 00:50:48 +0300

    init commit

---
+++ src/com/pb/shkarovskyi/hw2/Calculator.java
@@ -0,0 +1,9 @@
+package com.pb.shkarovskyi.hw2;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int x;
+        int y;
+        String sign = "+"; // + - * /
+    }
+}
