3d37d2b7bba802cca475b694a1a4381ef2ecb7e9
aleksandr-shkarovskij <92213389+aleksandr-shkarovskij@users.noreply.github.com>
2021-10-14 15:14:36 +0300

    Rename

---
+++ src/com/pb/shkarovskyi/hw2/Calculator_if.java
@@ -0,0 +1,41 @@
+package com.pb.shkarovskyi.hw2;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class Calculator_if {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(in);
+        String sign; // = "/"; // + - * /
+        int x;
+        int y;
+
+        System.out.println("Введите первое число");
+
+        x = scan.nextInt();
+
+        System.out.println("Введите второе число");
+
+        y = scan.nextInt();
+
+        System.out.println("Введите действие + - * /");
+
+        sign = scan.next();
+
+        if (Objects.equals(sign, "+")) {
+            System.out.print(x + " + " + y + " = " + (x + y));
+        } else if (Objects.equals(sign, "-")) {
+            System.out.print(x + " - " + y + " = " + (x - y));
+        } else if (Objects.equals(sign, "*")) {
+            System.out.print(x + " * " + y + " = " + (x * y));
+        } else if (Objects.equals(sign, "/")) {
+            if (y == 0)
+                System.out.println("На 0 делить нельзя, попробуйте заново");
+            System.out.print(x + " / " + y + " = " + (x / y));
+        } else {
+            System.out.print("Неверный ввод");
+        }
+    }
+}
