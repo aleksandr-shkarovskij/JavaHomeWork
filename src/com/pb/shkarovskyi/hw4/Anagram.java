d709742e1365c15dd1e6cea96049546bf0645239
aleksandr-shkarovskij <92213389+aleksandr-shkarovskij@users.noreply.github.com>
2021-10-31 01:11:28 +0300

    upd

--- src/com/pb/shkarovskyi/hw4/Anagram.java
+++ src/com/pb/shkarovskyi/hw4/Anagram.java
@@ -1,4 +1,10 @@
 package com.pb.shkarovskyi.hw4;
 
 public class Anagram {
+    public static void main(String[] args) {
+        char[] chars = "qwerty".toCharArray();
+        for (char o: chars) {
+            System.out.println(o);
+        }
+    }
 }
