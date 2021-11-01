30c75fa9447d1fc3f31725aa71ecf64f16bd8352
aleksandr-shkarovskij <92213389+aleksandr-shkarovskij@users.noreply.github.com>
2021-11-01 23:26:04 +0200

    init commit

---
+++ src/com/pb/shkarovskyi/hw5/Reader.java
@@ -0,0 +1,10 @@
+package com.pb.shkarovskyi.hw5;
+
+public class Reader {
+    String readerName;
+    int readerId;
+    String readerFaculty;
+    String readerBirthday;
+    int readerNumber;
+
+}
